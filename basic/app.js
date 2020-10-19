412a4f4818d6cf0ed12cf7d4731e273384629aed
moe akimoto <akimoe0503@gmail.com>
2020-10-19 20:46:42 +0900

    JC：授業と宿題

---
+++ basic/app.js
@@ -0,0 +1,92 @@
+// 文字列セブ島と留学を連結した結果をconsoleに表示してください。
+console.log('セブ島' + '留学');
+
+// 以下の計算の結果をconsoleに表示してください。
+// ① 5 たす 8
+// ② 4 ひく 2
+// ③ 8 かける 2
+// ④ 6 わる 3
+// ⑤ 9 わる 2の余り
+
+console.log('① 5 たす 8=' + String (5 + 8));
+console.log(4 - 2);
+console.log(8 * 2);
+console.log(6 / 3);
+console.log(9 % 2);
+
+// 変数countryを作成して、文字列Japanを代入してください。
+// 変数countryをconsoleに表示してください。
+// 1で作成した変数countryに文字列Philippinesを代入してください。
+// 変数countryをconsoleに表示してください。
+
+let country = 'Japan';
+console.log(country);
+country = 'Philippines'
+console.log(country);
+
+let schoolName = 'NexSeed';
+console.log(`私の所属先は、${schoolName}です`)
+
+// 変数jobを作成して、文字列engineerを代入してください。
+// テンプレートリテラルを使用して、私の仕事は、engineerですとconsoleに表示してください。
+
+let job = `engineer`;
+console.log(`私の仕事は、${job}です`)
+
+// 変数applicationsを作成して、配列['LINE', 'Facebook', 'Twitter']を代入してください。
+// 変数applicationsをconsoleに表示してください。
+// 変数applicationsにInstagramを追加してください。
+// 変数applicationsの中のInstagramをconsoleに表示してください。
+
+let applications = ['LINE', 'Facebook', 'Twitter'];
+console.log(applications);
+applications.push('Instagram');
+// console.log(applications);
+// ↑でもOKだけど下のほうがいい
+console.log(applications[3]);
+
+// // 変数capitalCitiesを作成して、連想配列{ Japan: 'Tokyo', Philippines: 'Manila' }を代入してください。
+// 変数capitalCitiesをconsoleに表示してください。
+// 変数capitalCitiesにAmerica: Washingtonを追加してください。
+// 変数capitalCitiesの中のWashingtonをconsoleに表示してください。
+
+let capitalCities = { Japan: 'Tokyo', Philippines: 'Manila' };
+console.log(capitalCities);
+capitalCities.America = 'Washington';
+console.log(capitalCities);
+
+// 宿題
+// クラスのみんなも名簿を2次元配列で作る。
+// 配列の中に、連想配列のかたち。
+// リストの0番目の人の名前をconsoleで表示する。
+// リストの3番目人の出身地をconsoleで表示する。
+// リストの４番目の人の趣味をconsoleで表示する。
+
+let studentList =[
+	{name:"Seedくん",from:"セブ島",hobby:"サーフィン"},
+	{name:"えりんごさん",from:"青森",hobby:"旅"},
+	{name:"あいちゃん",from:"沖縄",hobby:"旅行"},
+	{name:"クォッカ",from:"オーストラリア",hobby:"人と写真を撮る"},
+	{name:"えりぴょん",from:"和歌山",hobby:"もえぴーの専属カメラマン"},
+	{name:"めぐりん",from:"東京",hobby:"アウトドア"},
+	{name:"ゆうこりん",from:"沖縄",hobby:"写真"},
+	{name:"もえ",from:"山形",hobby:"韓ドラ"},
+	{name:"しょうさん",from:"岩手",hobby:"スノボしながらナンパ"},
+	{name:"ゆうだい",from:"大阪",hobby:"えいこうちゃんのファンクラブサイト作り"},
+	{name:"ゆた",from:"千葉",hobby:"田舎廻り"},
+	{name:"ルイス",from:"ペルー",hobby:"ヘアセット"},
+	{name:"このみん",from:"千葉",hobby:"ディズニー"},
+	{name:"よっしー",from:"京都",hobby:"デリバリー"}
+];
+console.log(studentList);
+console.log(studentList[0].name);
+console.log(studentList[3].from);
+console.log(studentList[4].hobby);
+
+
+
+
+
+
+
+
